50da160d84567b18ac0a013970a0987a245ba236
Raul Moreno <rauledmore98@outlook.com>
2025-02-05 19:20:55 -0500

    Cambios en el home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Home() {
+  // Cambiar el valor de 'useState' en la rama 'kakaroto'
+  const [data, setData] = useState(42);  // Cambié de 9 a 42
+
+  console.log(data);
+
   return (
     <main className="mb-52">
-      
+      {data}
     </main>
   );
 }
