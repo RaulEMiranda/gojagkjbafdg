5975edb5440985aef185a4bad7604b804831baac
Raul Moreno <rauledmore98@outlook.com>
2025-02-05 19:12:26 -0500

    Cambios en el Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState(9);
+
+  console.log(data);
+
   return (
     <main className="mb-52">
-      
+      {data}
     </main>
   );
 }
